e1f6f1a619495364b06c34d8672251b985641021
Luca Scalet <TamSchnow265@proton.me>
2023-08-15 16:30:02 +0200

    Update flatpaks.sh

--- flatpaks.sh
+++ flatpaks.sh
@@ -1,23 +1,11 @@
 flatpak install -y \
-  com.atlauncher.ATLauncher\
-  com.discordapp.Discord \
-  com.endlessnetwork.aqueducts \
   com.github.Anuken.Mindustry \
-  com.github.jms55.Sandbox \
-  com.github.lxgr_linux.pokete \
   com.github.tchx84.Flatseal \
-  com.heroicgameslauncher.hgl \
-  com.jetbrains.IntelliJ-IDEA-Community \
   com.visualstudio.code \
-  in.p1x.TanksOfFreedom \
-  info.colobot.Colobot \
   io.github.fabiangreffrath.Doom \
   net.blockbench.Blockbench \
-  net.lutris.Lutris \
   org.blender.Blender \
   org.gimp.GIMP \
-  page.codeberg.JakobDev.jdNBTExplorer \
-  org.kde.knetwalk \
   org.prismlauncher.PrismLauncher \
   it.mijorus.gearlever \
   org.mozilla.Thunderbird
