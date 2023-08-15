c3f2c49e9ebb7cefb0dc1d8dd2bdff228ebaadf2
Luca Scalet <TamSchnow265@proton.me>
2023-08-15 17:24:19 +0200

    Update FedUp.sh

--- FedUp.sh
+++ FedUp.sh
@@ -11,13 +11,6 @@ curl "https://raw.githubusercontent.com/schnow265/FedoraSetup/main/flatpaks.sh" 
 # Install RPMs & add RPMFusion
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-curl "https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm" -o chrome.rpm
-
-dnf install chrome.rpm
-
-
-
-
 # Upgrade the System to the latest version
 dnf offline-upgrade download -y
 echo "Run 'sudo dnf offline-upgrade reboot'"
